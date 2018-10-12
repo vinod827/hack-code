5f35e2cd72b4c033759ce8001dba956e366ad4e9
I'm a Superman!!! <24762720+vinod827@users.noreply.github.com>
2018-10-12 15:49:07 +0530

    updated code

--- src/main/java/com/mythreads/mythreads/UnencryptText.java
+++ src/main/java/com/mythreads/mythreads/UnencryptText.java
@@ -5,28 +5,15 @@ import org.apache.commons.logging.LogFactory;
 
 public class UnencryptText {
   final static Log logger = LogFactory.getLog(UnencryptText.class);
-
-/*
-  public void main(String[] args){
-    String encryptedString = "world";
-    UnencryptText unencryptText = new UnencryptText();
-    logger.info("Encrypted text: "+encryptedString);
-    logger.info("Unencrypted text: "+unencryptText.getUnencryptedText(encryptedString));
-  }
-*/
-
-
+  
   public String getUnencryptedText(String encryptedText){
     StringBuffer word = new StringBuffer();
-
     for(int i=0; i<encryptedText.length(); i++){
       int charValue = encryptedText.charAt(i);
       String nextChar = String.valueOf((char) (charValue + 3));
       word.append(nextChar);
     }
-
     return word.toString();
-
   }
-
+  
 }
